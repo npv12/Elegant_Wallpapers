362789bd11ae688652b92baf92e29174dfd18551
Pranav <npv12@iitbbs.ac.in>
2021-01-21 14:49:48 +0530

    Onesignal support
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createStackNavigator,CardStyleInterpolators, } from '@react-navigation/stack';
+import OneSignal from 'react-native-onesignal';
+
 import Explore from './src/screens/Explore';
 import Collections from './src/screens/Collections';
 import { NavigationContainer } from '@react-navigation/native';
@@ -11,6 +13,7 @@ import Settings from './src/screens/Settings'
 import Fav from './src/screens/favorite';
 import Test from './src/screens/test';
 import ThemeManager from './src/themes';
+import { app_id } from './constants';
 
 const Tab = createMaterialTopTabNavigator();
 function TopTabs() {
@@ -24,7 +27,7 @@ function TopTabs() {
 
 const Stack = createStackNavigator();
 
-function App () {
+function HomeScreen () {
   return (
     <>
     <ThemeManager>
@@ -45,5 +48,99 @@ function App () {
     </>
   );
 };
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        name: props.name,
+        isSubscribed: false,
+        requiresPrivacyConsent: false,
+        isLocationShared: false,
+        inputValue: "",
+        consoleValue: ""
+    }
+}
+
+async componentDidMount() {
+  /* O N E S I G N A L   S E T U P */
+  OneSignal.setAppId(app_id);
+  OneSignal.setLogLevel(6, 0);
+  OneSignal.setRequiresUserPrivacyConsent(this.state.requiresPrivacyConsent);
+  OneSignal.promptForPushNotificationsWithUserResponse(response => {
+      this.OSLog("Prompt response:", response);
+  });
+
+  /* O N E S I G N A L  H A N D L E R S */
+  OneSignal.setNotificationWillShowInForegroundHandler(notifReceivedEvent => {
+      this.OSLog("OneSignal: notification will show in foreground:", notifReceivedEvent);
+      let notif = notifReceivedEvent.getNotification();
+
+      const button1 = {
+          text: "Cancel",
+          onPress: () => { notifReceivedEvent.complete(); },
+          style: "cancel"
+      };
+
+      const button2 = { text: "Complete", onPress: () => { notifReceivedEvent.complete(notif); }};
+
+        Alert.alert("Complete notification?", "Test", [ button1, button2], { cancelable: true });
+    });
+    OneSignal.setNotificationOpenedHandler(notification => {
+        this.OSLog("OneSignal: notification opened:", notification);
+    });
+    OneSignal.setInAppMessageClickHandler(event => {
+        this.OSLog("OneSignal IAM clicked:", event);
+    });
+    OneSignal.addEmailSubscriptionObserver((event) => {
+        this.OSLog("OneSignal: email subscription changed: ", event);
+    });
+    OneSignal.addSubscriptionObserver(event => {
+        this.OSLog("OneSignal: subscription changed:", event);
+        this.setState({ isSubscribed: event.to.isSubscribed})
+    });
+    OneSignal.addPermissionObserver(event => {
+        this.OSLog("OneSignal: permission changed:", event);
+    });
+    const state = await OneSignal.getDeviceState();
+
+    this.setState({
+        name : state.emailAddress,
+        isSubscribed : state.isSubscribed
+    });
+}
+
+componentWillUnmount() {
+    OneSignal.clearHandlers();
+}
+
+  OSLog = (message, optionalArg) => {
+
+    if (optionalArg) {
+        message = message + JSON.stringify(optionalArg);
+    }
+
+    inputChange = (text) => {
+      this.setState({ inputValue: text })
+  }
+
+    console.log(message);
+
+    let consoleValue;
+
+    if (this.state.consoleValue) {
+        consoleValue = this.state.consoleValue+"\n"+message
+    } else {
+        consoleValue = message;
+    }
+    this.setState({ consoleValue });
+  }
+
+  render()
+  {
+    return <HomeScreen/>
+  }
+}
 
 export default App;
--- src/screens/test.js
+++ src/screens/test.js
@@ -1,34 +1,115 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ThemeManager, { useTheme } from '../themes'
-import { Switch,Text } from 'react-native'
-import styled from 'styled-components/native'
-
-const Container = styled.View`
-  background: ${props => props.theme.background};
-`
-
-const Title = styled.Text`
-  color: ${props => props.theme.text};
-`
+import { Switch,Text,View } from 'react-native'
+import OneSignal from 'react-native-onesignal';
+import { app_id } from '../../constants';
 
 function HomeScreen() {
     const theme = useTheme()
     console.log(theme)
     return (
-      <Container style={{flex:1, alignItems: 'center',justifyContent: 'flex-end'}}>
-        <Title style={{fontSize:42,}}>Crowdbotics app</Title>
+      <View style={{flex:1, alignItems: 'center',justifyContent: 'flex-end'}}>
+        <Text style={{fontSize:42,}}>Crowdbotics app</Text>
         <Switch
           value={theme.mode === 'dark'}
           onValueChange={value => theme.setMode(value ? 'dark' : 'light')}
         />
-      </Container>
+      </View>
     )
   }
 
-  function Test() {
-    return (
-        <HomeScreen />
-    )
+  class Test extends Component {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+          name: props.name,
+          isSubscribed: false,
+          requiresPrivacyConsent: false,
+          isLocationShared: false,
+          inputValue: "",
+          consoleValue: ""
+      }
+  }
+
+  async componentDidMount() {
+    /* O N E S I G N A L   S E T U P */
+    OneSignal.setAppId(app_id);
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setRequiresUserPrivacyConsent(this.state.requiresPrivacyConsent);
+    OneSignal.promptForPushNotificationsWithUserResponse(response => {
+        this.OSLog("Prompt response:", response);
+    });
+
+    /* O N E S I G N A L  H A N D L E R S */
+    OneSignal.setNotificationWillShowInForegroundHandler(notifReceivedEvent => {
+        this.OSLog("OneSignal: notification will show in foreground:", notifReceivedEvent);
+        let notif = notifReceivedEvent.getNotification();
+
+        const button1 = {
+            text: "Cancel",
+            onPress: () => { notifReceivedEvent.complete(); },
+            style: "cancel"
+        };
+
+        const button2 = { text: "Complete", onPress: () => { notifReceivedEvent.complete(notif); }};
+
+          Alert.alert("Complete notification?", "Test", [ button1, button2], { cancelable: true });
+      });
+      OneSignal.setNotificationOpenedHandler(notification => {
+          this.OSLog("OneSignal: notification opened:", notification);
+      });
+      OneSignal.setInAppMessageClickHandler(event => {
+          this.OSLog("OneSignal IAM clicked:", event);
+      });
+      OneSignal.addEmailSubscriptionObserver((event) => {
+          this.OSLog("OneSignal: email subscription changed: ", event);
+      });
+      OneSignal.addSubscriptionObserver(event => {
+          this.OSLog("OneSignal: subscription changed:", event);
+          this.setState({ isSubscribed: event.to.isSubscribed})
+      });
+      OneSignal.addPermissionObserver(event => {
+          this.OSLog("OneSignal: permission changed:", event);
+      });
+      const state = await OneSignal.getDeviceState();
+
+      this.setState({
+          name : state.emailAddress,
+          isSubscribed : state.isSubscribed
+      });
+  }
+
+  componentWillUnmount() {
+      OneSignal.clearHandlers();
+  }
+
+    OSLog = (message, optionalArg) => {
+
+      if (optionalArg) {
+          message = message + JSON.stringify(optionalArg);
+      }
+
+      inputChange = (text) => {
+        this.setState({ inputValue: text })
+    }
+
+      console.log(message);
+
+      let consoleValue;
+
+      if (this.state.consoleValue) {
+          consoleValue = this.state.consoleValue+"\n"+message
+      } else {
+          consoleValue = message;
+      }
+      this.setState({ consoleValue });
+    }
+
+    render()
+    {
+      return <HomeScreen/>
+    }
   }
   
   export default Test
