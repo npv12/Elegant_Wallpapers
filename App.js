ece8a68f9364ba8e25d7d1314fefbf00a8994210
Pranav <npv12@iitbbs.ac.in>
2021-08-16 00:31:29 +0530

    admob: finish transtition
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   StatusBar,
   View
 } from 'react-native';
+import admob, { MaxAdContentRating, } from '@react-native-firebase/admob';
 
 import Explore from './src/screens/Explore';
 import Collections from './src/screens/Collections';
@@ -202,6 +203,21 @@ async componentDidMount() {
   OneSignal.promptForPushNotificationsWithUserResponse(response => {
     //  this.OSLog("Prompt response:", response);
   });
+  admob()
+    .setRequestConfiguration({
+      // Update all future requests suitable for parental guidance
+      maxAdContentRating: MaxAdContentRating.PG,
+
+      // Indicates that you want your content treated as child-directed for purposes of COPPA.
+      tagForChildDirectedTreatment: true,
+
+      // Indicates that you want the ad request to be handled in a
+      // manner suitable for users under the age of consent.
+      tagForUnderAgeOfConsent: true,
+    })
+    .then(() => {
+      console.log("Config set successfully")
+    });
 
   OneSignal.setNotificationWillShowInForegroundHandler(notifReceivedEvent => {
      // this.OSLog("OneSignal: notification will show in foreground:", notifReceivedEvent);
--- src/components/Advert.js
+++ src/components/Advert.js
@@ -1,14 +1,18 @@
+import { AdEventType, InterstitialAd } from '@react-native-firebase/admob';
 import { InterstitialAdManager,AdSettings } from 'react-native-fbads';
-import { ADVERT_CAP_TIME, FB_PLACEMENT_AD } from '../constants';
+import { ADVERT_CAP_TIME, FB_PLACEMENT_AD, ADMOB_UNIT_ID } from '../constants';
 
 var shouldShowAd = true
+const interstitialAd = InterstitialAd.createForAdRequest(ADMOB_UNIT_ID);
 
 export default function loadAd(){
     console.log(shouldShowAd, "Should the ad be shown?")
     if(shouldShowAd){
-        InterstitialAdManager.showPreloadedAd(FB_PLACEMENT_AD)
-        .then((didClick) => {console.log(didClick,"Show")})
-        .catch((error) => {console.log(error,"Show")});
+        try{
+            interstitialAd.show();
+        } catch(e) {
+            console.log(e)
+        }
         preloadAd()
         shouldShowAd=false;
         setTimeout(function(){
@@ -18,7 +22,14 @@ export default function loadAd(){
 }
 
 export function preloadAd(){
+    try{
+        interstitialAd.load();
+    } catch(e) {
+        console.log(e)
+    }
+    
+    /*
     InterstitialAdManager.preloadAd(FB_PLACEMENT_AD)
     .then((didClick) => {console.log(didClick,"PreLoad")})
-    .catch((error) => {console.log(error,"PreLoad")});
+    .catch((error) => {console.log(error,"PreLoad")});*/
 }
