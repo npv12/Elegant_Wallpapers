36cb148c359ec40cdd1fe691a52843b925b5ca6f
Pranav <npv12@iitbbs.ac.in>
2022-02-04 12:13:23 +0530

    Refactor: update theme with the newest changes
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,49 +1,41 @@
-import React, { createContext, useState, useEffect } from 'react'
-import { StatusBar } from 'react-native'
-import { ThemeProvider } from 'styled-components/native'
-import { Appearance, AppearanceProvider } from 'react-native-appearance'
+import React, { createContext, useState, useEffect } from "react";
+import { Appearance, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
 
-import lightTheme from './light'
-import darkTheme from './dark'
-const defaultMode = Appearance.getColorScheme() || 'light'
+import lightTheme from "./light";
+import darkTheme from "./dark";
+const defaultMode = Appearance.getColorScheme();
 const ThemeContext = createContext({
-    mode: defaultMode,
-    setMode: mode => console.log(mode)
-  })
-  
-  export const useTheme = () => React.useContext(ThemeContext)
+	mode: defaultMode,
+	setMode: (mode) => console.log(mode),
+});
 
-  const ManageThemeProvider = ({ children }) => {
-    const [themeState, setThemeState] = useState(defaultMode)
-    const setMode = mode => {
-      setThemeState(mode)
-    }
-    useEffect(() => {
-      const subscription = Appearance.addChangeListener(({ colorScheme }) => {
-        setThemeState(colorScheme)
-      })
-      return () => subscription.remove()
-    }, [])
-    
-    return (
-      <ThemeContext.Provider value={{ mode: themeState, setMode }}>
-        <ThemeProvider
-          theme={themeState === 'dark' ? darkTheme.theme : lightTheme.theme}>
-          <>
-            <StatusBar
-              barStyle={themeState === 'dark' ? 'dark-content' : 'light-content'}
-            />
-            {children}
-          </>
-        </ThemeProvider>
-      </ThemeContext.Provider>
-    )
-  }
+export const useTheme = () => React.useContext(ThemeContext);
 
-  const ThemeManager = ({ children }) => (
-    <AppearanceProvider>
-      <ManageThemeProvider>{children}</ManageThemeProvider>
-    </AppearanceProvider>
-  )
-  
-  export default ThemeManager
+const ManageThemeProvider = ({ children }) => {
+	const [themeState, setThemeState] = useState(defaultMode);
+	const setMode = (mode) => {
+		setThemeState(mode);
+	};
+
+	return (
+		<ThemeContext.Provider value={{ mode: themeState, setMode }}>
+			<ThemeProvider
+				theme={themeState === "dark" ? darkTheme.theme : lightTheme.theme}
+			>
+				<>
+					<StatusBar
+						barStyle={themeState === "dark" ? "dark-content" : "light-content"}
+					/>
+					{children}
+				</>
+			</ThemeProvider>
+		</ThemeContext.Provider>
+	);
+};
+
+const ThemeManager = ({ children }) => (
+	<ManageThemeProvider>{children}</ManageThemeProvider>
+);
+
+export default ThemeManager;
