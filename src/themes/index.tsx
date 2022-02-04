de77c62b7d26c986888c44aec5eaf964475bffbb
Pranav <npv12@iitbbs.ac.in>
2022-02-04 23:09:58 +0530

    refactor: Move themes to tsx and make it respond to theme change
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/themes/index.tsx
+++ src/themes/index.tsx
@@ -1,34 +1,40 @@
-import React, { createContext, useState, useEffect } from "react";
-import { Appearance, StatusBar } from "react-native";
+import React, { createContext, useState, useEffect, useCallback } from "react";
+import { StatusBar, Appearance } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 
 import lightTheme from "./light";
 import darkTheme from "./dark";
-const defaultMode = Appearance.getColorScheme();
+const defaultMode = Appearance.getColorScheme() || "light";
 const ThemeContext = createContext({
 	mode: defaultMode,
 	setMode: (mode) => console.log(mode),
 });
 
 export const useTheme = () => React.useContext(ThemeContext);
-
 const ManageThemeProvider = ({ children }) => {
 	const [themeState, setThemeState] = useState(defaultMode);
-	const setMode = (mode) => {
+	const setMode = (mode: any) => {
 		setThemeState(mode);
 	};
+
+	const changeOnListener = useCallback(() => {
+		setThemeState(Appearance.getColorScheme());
+	}, []);
+
+	useEffect(() => {
+		Appearance.addChangeListener(changeOnListener);
+		return () => Appearance.removeChangeListener(changeOnListener);
+	}, []);
 
 	return (
 		<ThemeContext.Provider value={{ mode: themeState, setMode }}>
 			<ThemeProvider
 				theme={themeState === "dark" ? darkTheme.theme : lightTheme.theme}
 			>
-				<>
-					<StatusBar
-						barStyle={themeState === "dark" ? "dark-content" : "light-content"}
-					/>
-					{children}
-				</>
+				<StatusBar
+					barStyle={themeState === "dark" ? "dark-content" : "light-content"}
+				/>
+				{children}
 			</ThemeProvider>
 		</ThemeContext.Provider>
 	);
