4a9f26078e5903741aacf5d832afc6efe762dfe4
Pranav <npv12@iitbbs.ac.in>
2022-02-04 12:15:04 +0530

    kill ads for now
    * till i do it in a much better manner
    
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/components/Advert.js
+++ src/components/Advert.js
@@ -1,33 +1,32 @@
-import { AdEventType, InterstitialAd } from '@react-native-firebase/admob';
-import { ADVERT_CAP_TIME, FB_PLACEMENT_AD, ADMOB_UNIT_ID } from '../constants';
+import { AdEventType, InterstitialAd } from "@react-native-firebase/admob";
+import { ADVERT_CAP_TIME, FB_PLACEMENT_AD, ADMOB_UNIT_ID } from "../constants";
 
-var shouldShowAd = true
+var shouldShowAd = true;
 const interstitialAd = InterstitialAd.createForAdRequest(ADMOB_UNIT_ID);
 
-export default function loadAd(){
-    console.log(shouldShowAd, "Should the ad be shown?")
-    if(shouldShowAd){
-        try{
-            interstitialAd.show();
-        } catch(e) {
-            console.log(e)
-        }
-        preloadAd()
-        shouldShowAd=false;
-        setTimeout(function(){
-            shouldShowAd = true
-            },ADVERT_CAP_TIME/2)
-    }
+export default function loadAd() {
+	console.log(shouldShowAd, "Should the ad be shown?");
+	// if(shouldShowAd){
+	//     try{
+	//         interstitialAd.show();
+	//     } catch(e) {
+	//         console.log(e)
+	//     }
+	//     preloadAd()
+	//     shouldShowAd=false;
+	//     setTimeout(function(){
+	//         shouldShowAd = true
+	//         },ADVERT_CAP_TIME/2)
+	// }
 }
 
-export function preloadAd(){
-    try{
-        interstitialAd.load();
-    } catch(e) {
-        console.log(e)
-    }
-    
-    /*
+export function preloadAd() {
+	// try{
+	//     interstitialAd.load();
+	// } catch(e) {
+	//     console.log(e)
+	// }
+	/*
     InterstitialAdManager.preloadAd(FB_PLACEMENT_AD)
     .then((didClick) => {console.log(didClick,"PreLoad")})
     .catch((error) => {console.log(error,"PreLoad")});*/
