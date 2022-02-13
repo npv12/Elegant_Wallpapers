5b01357f88035c1c235216bc0fba3c9f318448a8
Pranav <npv12@iitbbs.ac.in>
2022-02-14 01:37:31 +0530

    chore: Add code to initialise mediation
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- android/app/src/main/java/com/madness/wallz/app/MainActivity.java
+++ android/app/src/main/java/com/madness/wallz/app/MainActivity.java
@@ -3,19 +3,31 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
+import java.util.*;
 
 import com.facebook.react.ReactActivity;
 
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.AdapterStatus;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
   @Override
   protected void onCreate(Bundle savedInstanceState){
     SplashScreen.show(this);
     super.onCreate(savedInstanceState);
+    MobileAds.initialize(this, new OnInitializationCompleteListener() {
+        @Override
+        public void onInitializationComplete(InitializationStatus initializationStatus) {
+            Map<String, AdapterStatus> statusMap = initializationStatus.getAdapterStatusMap();
+            for (String adapterClass : statusMap.keySet()) {
+                // Initialise an adapter for mediation
+                AdapterStatus status = statusMap.get(adapterClass);
+            }
+        }
+    });
   }
 
   @Override
