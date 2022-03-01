7d5aaca3e287527a95f80f16b5b40a37f097941a
Pranav <npv12@iitbbs.ac.in>
2022-03-01 12:35:08 +0530

    fixup: Set theme on first launch as well
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/Themes/index.tsx
+++ src/Themes/index.tsx
@@ -1,30 +1,26 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import React, { useState, useEffect, useContext } from "react";
-import { StatusBar, Appearance } from "react-native";
+import { StatusBar, Appearance, NativeEventSubscription } from "react-native";
 import { TypeThemeContext } from "../types/themes";
 import darkColor from "./dark";
 import lightColor from "./light";
 import { ThemeContext } from "./ThemeContext";
-var listener
 
 const Themes = ({ children }) => {
 	const { setTheme, setMode, mode } = useContext<TypeThemeContext>(ThemeContext)
 
 	async function setThemeFromStorage() {
 		var themeFromStorage = await AsyncStorage.getItem("theme");
-		listener = Appearance.addChangeListener(() => {
+		Appearance.addChangeListener(() => {
 			setMode(Appearance.getColorScheme())
 			setTheme(Appearance.getColorScheme() === "light" ? lightColor : darkColor)
-			console.log("The color scheme is " + Appearance.getColorScheme())
 		})
 	}
 	useEffect(() => {
 		setThemeFromStorage();
+		setMode(Appearance.getColorScheme())
+		setTheme(Appearance.getColorScheme() === "light" ? lightColor : darkColor)
 	}, [])
-
-	useEffect(() => () => {
-		listener.remove()
-	})
 
 	return (
 		<>
