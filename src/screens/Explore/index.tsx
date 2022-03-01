cfaab46115dc6964773b788d04e22253af0f9e06
Pranav <npv12@iitbbs.ac.in>
2022-03-01 14:33:28 +0530

    cleanup: styles in explore.tsx
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/screens/Explore/index.tsx
+++ src/screens/Explore/index.tsx
@@ -5,7 +5,6 @@ import {
 	Dimensions,
 	TouchableOpacity,
 	Linking,
-	Animated,
 } from "react-native";
 import {
 	STANDARD_HEIGHT,
@@ -16,7 +15,6 @@ import {
 	FREE_APP,
 } from "../../constants";
 import SplashScreen from "react-native-splash-screen";
-import { useIsFocused } from "@react-navigation/native";
 import { Text, View } from "../../components/StyledComponents";
 import { TypeThemeContext } from "../../types/themes";
 import { ThemeContext } from "../../Themes/ThemeContext";
@@ -31,9 +29,6 @@ const Explore = () => {
 	const { theme, mode, setMode } = useContext<TypeThemeContext>(ThemeContext);
 	const [data, setData] = useState([]);
 	const [updateState, setUpdateState] = useState(0);
-	const [fadeAnimation, setFadeAnimation] = useState(new Animated.Value(0));
-	const [offset, setOffset] = useState(0);
-	const focused = useIsFocused();
 
 	async function getData() {
 		fetch(WALL_URL, {
@@ -64,27 +59,7 @@ const Explore = () => {
 
 	useEffect(() => {
 		getData();
-		//scrollToOffset(offset)
-		return function () { };
 	}, []);
-
-	useEffect(() => {
-		if (focused) fadeIn();
-		return function () { };
-	}, [focused]);
-
-	function fadeIn() {
-		Animated.timing(fadeAnimation, {
-			toValue: 1,
-			duration: 800,
-			useNativeDriver: true,
-		}).start();
-	}
-
-	function handleScroll(event) {
-		let offsety = event.nativeEvent.contentOffset.y;
-		setOffset(offsety);
-	}
 
 	if (updateState != 0) {
 		if (updateState == 2)
@@ -146,23 +121,14 @@ const Explore = () => {
 		return (
 			<>
 				<View style={styles.container}>
-					<Animated.View
-						style={[
-							styles.container,
-							{
-								opacity: fadeAnimation,
-							},
-						]}
-					>
-						<StatusBar
-							translucent={true}
-							backgroundColor={"transparent"}
-							barStyle={mode == "dark" ? "light-content" : "dark-content"}
-						/>
-						<View style={{ ...styles.container }}>
-							<ScrollableCollection data={data} />
-						</View>
-					</Animated.View>
+					<StatusBar
+						translucent={true}
+						backgroundColor={"transparent"}
+						barStyle={mode == "dark" ? "light-content" : "dark-content"}
+					/>
+					<View style={{ ...styles.container }}>
+						<ScrollableCollection data={data} />
+					</View>
 				</View>
 			</>
 		);
@@ -174,81 +140,6 @@ const Explore = () => {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-	},
-	icon: {
-		paddingHorizontal: 10 * scaleWidth,
-	},
-	searchBox: {
-		justifyContent: "center",
-		height: 50 * scaleHeight,
-		width: 50 * scaleWidth,
-		borderRadius: 70,
-		elevation: 10,
-		shadowColor: "#fff",
-		position: "absolute",
-		opacity: 1,
-		bottom: 45 * scaleHeight,
-		right: 40 * scaleWidth,
-	},
-	headerContainer: {},
-	bottomTab: {
-		justifyContent: "center",
-		alignItems: "flex-start",
-		shadowOpacity: 1,
-		height: 70 * scaleHeight,
-		position: "absolute",
-		bottom: 0,
-		width: "100%",
-		borderTopEndRadius: 30,
-		shadowColor: "#000",
-		shadowOffset: {
-			width: 0,
-			height: 2,
-		},
-		shadowRadius: 3.84,
-		elevation: 5,
-	},
-	Wall: {
-		width: (windowWidth / 2) * 0.88,
-		height: 250 * scaleHeight,
-		borderRadius: 5,
-		borderTopRightRadius: 5,
-	},
-	wallBoundary: {
-		flex: 1,
-		margin: 8,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	openButton: {
-		backgroundColor: "#F194FF",
-		borderRadius: 20,
-		padding: 10 * scaleHeight,
-		elevation: 2,
-	},
-	textStyle: {
-		color: "white",
-		fontWeight: "bold",
-		textAlign: "center",
-	},
-	modalText: {
-		marginBottom: 15 * scaleHeight,
-		textAlign: "center",
-	},
-	modalItem: {
-		paddingLeft: 25 * scaleWidth,
-		flexDirection: "row",
-		marginVertical: 5 * scaleHeight,
-		width: windowWidth,
-		justifyContent: "flex-start",
-	},
-	pill: {
-		backgroundColor: "#898989",
-		height: 5 * scaleHeight,
-		width: 40 * scaleWidth,
-		borderRadius: 10,
-		marginBottom: 15,
-		alignSelf: "center",
 	},
 });
 
