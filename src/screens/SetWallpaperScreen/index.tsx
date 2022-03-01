dd0ba81d3abff72c2bb29a5760281ffd13fa2e72
Pranav <npv12@iitbbs.ac.in>
2022-03-01 22:33:21 +0530

    Slightly cleanup setwallscreen
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/screens/SetWallpaperScreen/index.tsx
+++ src/screens/SetWallpaperScreen/index.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from "react";
-import { StatusBar, Image, View } from "react-native";
+import React, { useState, useContext } from "react";
+import { StatusBar, View } from "react-native";
 import Loader from "../../components/Loader";
-import ImageColors from "react-native-image-colors";
 import LoadingImage from "../../components/LoadingImage";
 import { TypeAppContext } from "../../types";
 import { AppContext } from "../../context/AppContext";
@@ -9,41 +8,9 @@ import styles from "./styles";
 import BottomTab from "./BottomTab";
 
 const SetWallpaperScreen = ({ route }) => {
-	const { theme, mode } = useContext<TypeAppContext>(AppContext);
+	const { mode } = useContext<TypeAppContext>(AppContext);
 	const { item } = route.params;
-	const [iconColor, setIconColor] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
-
-	useEffect(() => {
-		if (mode == "dark" && !iconColor) setIconColor(true);
-		else if (mode == "light" && iconColor) setIconColor(false);
-		retrieveData();
-		return function () {};
-	}, []);
-
-	//retrieve data from storage
-	async function retrieveData() {
-		//extract colors
-		var col: any = {
-			average: "#787C83",
-			darkMuted: "#101818",
-			darkVibrant: "#000000",
-			dominant: "#101818",
-			lightMuted: "#E8E8F0",
-			lightVibrant: "#000000",
-			muted: "#788090",
-			platform: "android",
-			vibrant: "#000000",
-		};
-		col = await ImageColors.getColors(item.thumbnail, {
-			fallback: "#000000",
-			quality: "high",
-			pixelSpacing: 5,
-		});
-
-		//image size for better viewing. currently not in use
-		Image.getSize(item.thumbnail, (w, h) => {}).catch();
-	}
 
 	return (
 		<View style={{ flex: 1 }}>
