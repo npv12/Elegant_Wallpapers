900b8a2dc3dc4ddbf6d81a35cc1b61e8a8591999
Pranav <npv12@iitbbs.ac.in>
2022-03-07 10:35:54 +0530

    Cleanup advert.tsx and make it minimal
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- src/components/Advert.tsx
+++ src/components/Advert.tsx
@@ -1,82 +1,45 @@
-import { ADVERT_CAP_TIME,ADMOB_UNIT_ID } from "../constants";
-import mobileAds from 'react-native-google-mobile-ads';
-import { InterstitialAd, TestIds, AdEventType } from 'react-native-google-mobile-ads';
+import { ADMOB_UNIT_ID } from "../constants";
+import mobileAds from "react-native-google-mobile-ads";
+import { InterstitialAd, AdEventType } from "react-native-google-mobile-ads";
 
-
-
-var shouldShowAd = true;
-
-export function initAdmob(){
-  mobileAds()
-  .initialize()
-  .then((adapterStatuses) => {
-
-    // Initialization complete!
-    console.log("SDK INITIALIZED")
-    setTimeout(function(){
-      preloadAd()
-      },3000)
-	
-  });
+export function initAdmob() {
+	mobileAds()
+		.initialize()
+		.then((adapterStatuses) => {
+			// Initialization complete!
+			console.log("SDK INITIALIZED");
+			loadAdv();
+		});
 }
 
+const interstitial = InterstitialAd.createForAdRequest(ADMOB_UNIT_ID, {});
 
-
-  const interstitial = InterstitialAd.createForAdRequest(ADMOB_UNIT_ID, {
-	//requestNonPersonalizedAdsOnly: true,
-	//keywords: ['fashion', 'clothing'],
-  });
-
-  const eventListener = interstitial.onAdEvent(type => {
+interstitial.onAdEvent((type) => {
 	if (type === AdEventType.LOADED) {
-		console.log("AD LOADED")
+		console.log("AD LOADED");
 	}
-    if (type === AdEventType.ERROR) {
-      console.log("AD ERROR")
-    }
-    if (type === AdEventType.CLOSED) {
-      console.log("AD CLOSED")
-	 // shouldShowAd set to false after closing the ad
-      shouldShowAd=false;
-      console.log("SHOULD THE AD BE SHOWN - SET TO FALSE")
-    }
+	if (type === AdEventType.ERROR) {
+		console.log("AD ERROR");
+	}
+	if (type === AdEventType.CLOSED) {
+		console.log("AD CLOSED");
+	}
+});
 
-  
-  });
-
-  export default function loadAd(){
-    console.log(shouldShowAd, "SHOULD THE AD BE SHOWN?")
-
-    if(shouldShowAd){
-        try{
-            interstitial.show();
-        } catch(e) {
-            console.log(e)
-        }
-
-        setTimeout(function(){
-		// start preloadAd on a delay after trying to show the ad - better for fill ratio
-          preloadAd()
-          },ADVERT_CAP_TIME/3)
-
-        setTimeout(function(){
-
-		  // shouldShowAd set to true after the set cap time 
-            shouldShowAd = true
-            console.log("SHOULD THE AD BE SHOWN - SET TO TRUE")
-            },ADVERT_CAP_TIME)
-
-
-    }
+export default function showAdv() {
+	try {
+		interstitial.show();
+	} catch (e) {
+		console.log(e);
+	}
+	loadAdv();
 }
 
-export function preloadAd(){
-    try{
-      console.log("PRELOAD CALLED")
-        interstitial.load();
-    } catch(e) {
-        console.log(e)
-    }
-    
-   
+export function loadAdv() {
+	try {
+		console.log("PRELOAD CALLED");
+		interstitial.load();
+	} catch (e) {
+		console.log(e);
+	}
 }
